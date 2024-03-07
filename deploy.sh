a508304bfa9d634413d3621bf49fba4222096904
dwalter <dpwalter7@gmail.com>
2024-03-07 08:03:09 -0500

    wip: convert old bash script to python for deploying frontend and backend

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@ service_name=$(basename $(pwd))
 
 # cp the files to the appropriate locations
 
-# sync the frontend files
-mkdir -p /var/www/$service_name;
-cp frontend/* /var/www/$service_name/;
+# # sync the frontend files
+# mkdir -p /var/www/$service_name;
+# cp frontend/* /var/www/$service_name/;
 
-# sync the backend files
-mkdir -p /var/www/$service_name/backend;
-cp backend/* /var/www/self_system_app/backend/;
+# # sync the backend files
+# mkdir -p /var/www/$service_name/backend;
+# cp backend/* /var/www/self_system_app/backend/;
 
 
 # copy the backend service file to the appropriate location
@@ -19,24 +19,24 @@ cp backend/* /var/www/self_system_app/backend/;
 # cp backend_service/self_system_app_backend.service \
 #   /etc/systemd/system/self_system_app_backend.service;
 
-# # sync the nginx config file:
-# cp nginx/self_system_app \
-#   /etc/nginx/sites-available/self_system_app;
-# # symlink the nginx config file:
-# if [ ! -L /etc/nginx/sites-enabled/self_system_app ]; then
-#     sudo ln -s \
-#   /etc/nginx/sites-available/self_system_app \
-#   /etc/nginx/sites-enabled/self_system_app
-# fi
+# sync the nginx config file:
+cp nginx/self_system_app \
+  /etc/nginx/sites-available/self_system_app;
+# symlink the nginx config file:
+if [ ! -L /etc/nginx/sites-enabled/self_system_app ]; then
+    sudo ln -s \
+  /etc/nginx/sites-available/self_system_app \
+  /etc/nginx/sites-enabled/self_system_app
+fi
 
-# # restart the frontend and backend servers
+# restart the frontend and backend servers
 
-# # enable the backend service on boot:
-# sudo systemctl enable self_system_app_backend.service;
-# # restart the backend service that runs `python app.py`:
-# sudo systemctl restart self_system_app_backend.service;
+# enable the backend service on boot:
+sudo systemctl enable self_system_app_backend.service;
+# restart the backend service that runs `python app.py`:
+sudo systemctl restart self_system_app_backend.service;
 
-# # restart the nginx server which hosts the frontend
-# # also reverse proxies the backend
-# sudo systemctl enable nginx;
-# sudo systemctl restart nginx;
+# restart the nginx server which hosts the frontend
+# also reverse proxies the backend
+sudo systemctl enable nginx;
+sudo systemctl restart nginx;
--- simple_site.py
+++ simple_site.py
@@ -1,9 +1,9 @@
 import os
 from pkg_resources import resource_filename
-# use args
 import argparse
+import requests
 
-def handle_deploy(service_name):
+def handle_deploy(service_name, port, public_ip):
     build_dir = "build"
     os.makedirs(build_dir, exist_ok=True)
     template_backend_path = resource_filename('simple_site', 'templates/template_backend.service')
@@ -19,20 +19,79 @@ def handle_deploy(service_name):
         f.write(service_content)
 
     # run the deploy.sh script
-    os.system(f"bash deploy.sh {service_name}")
+    # os.system(f"bash deploy.sh {service_name}")
 
-def main(service_name, deploy):
+    # # sync the frontend files
+    # mkdir -p /var/www/$service_name;
+    # cp frontend/* /var/www/$service_name/;
+    os.makedirs(f"/var/www/{service_name}", exist_ok=True)
+    os.system(f"cp frontend/* /var/www/{service_name}/")
+
+    # sync the backend files
+    # mkdir -p /var/www/$service_name/backend;
+    # cp backend/* /var/www/self_system_app/backend/;
+    os.makedirs(f"/var/www/{service_name}/backend", exist_ok=True)
+    os.system(f"cp backend/* /var/www/{service_name}/backend/")
+
+    # create the nginx config from the template
+
+    if public_ip is None:
+        public_ip = get_public_ip()
+
+    nginx_template = None
+    with open("nginx_template", 'r') as f:
+        nginx_template = f.read()
+
+    nginx_config = nginx_template.replace('<service_name>', service_name)
+    nginx_config = nginx_config.replace('port', port)
+    nginx_config = nginx_config.replace('public_ip', public_ip)
+
+    with open(f"/etc/nginx/sites-available/{service_name}", 'w') as f:
+        f.write(nginx_config)
+
+    # # symlink the nginx config file:
+    # if [ ! -L /etc/nginx/sites-enabled/self_system_app ]; then
+    #     sudo ln -s \
+    # /etc/nginx/sites-available/self_system_app \
+    # /etc/nginx/sites-enabled/self_system_app
+    # fi
+    # if not os.path.exists(f"/etc/nginx/sites-enabled/{service_name}"):
+    os.symlink(
+        f"/etc/nginx/sites-available/{service_name}",
+        f"/etc/nginx/sites-enabled/{service_name}"
+    )
+
+    # enable the backend service on boot:
+    os.system(f"sudo systemctl enable {service_backend_name}.service")
+    # restart the backend service that runs `python app.py`:
+    os.system(f"sudo systemctl restart {service_backend_name}.service")
+
+    # restart the nginx server which hosts the frontend
+    # also reverse proxies the backend
+    os.system("sudo systemctl enable nginx")
+    os.system("sudo systemctl restart nginx")
+
+
+def get_public_ip():
+    response = requests.get('https://api.ipify.org')
+    return response.text
+
+def main(service_name, deploy, port, public_ip):
 
     if not deploy:
         return
 
-    handle_deploy(service_name)
+    handle_deploy(service_name, port, public_ip)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a simple site')
     parser.add_argument('service_name', type=str)
     parser.add_argument("deploy", type=bool, default=False)
+    parser.add_argument("port", type=int, default=8081)
+    parser.add_argument("public_ip", type=str, default=None)
 
     service_name = parser.parse_args().service_name
     deploy = parser.parse_args().deploy
-    main(service_name, deploy)
+    port = parser.parse_args().port
+    public_ip = parser.parse_args().public_ip
+    main(service_name, deploy, port, public_ip)
